f2aa5f730980baa72e6fba3667a882300dd2f618
RanaReebaal <113040207+RanaReebaal@users.noreply.github.com>
2022-11-09 10:55:01 +0500

    Shopping Mart Website

---
+++ shoppingmart/views.py
@@ -0,0 +1,92 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+import json
+from . import views
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contact(request):
+    name = request.GET.get('us_name')
+    age = request.GET.get('us_age')
+    city = request.GET.get('us_city')
+    job = request.GET.get('us_job')
+    if name:
+        if age:
+            if city:
+                if job:
+                    class model():
+                        def __init__(self, member, age, city, job):
+                            self.member = member
+                            self.age = age
+                            self.city = city
+                            self.job = job
+
+                        def member_name(self):
+                            return self.member
+
+                        def member_age(self):
+                            return self.age
+
+                        def member_city(self):
+                            return self.city
+
+                        def member_job(self):
+                            return self.job
+
+                    user = model(name, age, city, job)
+
+                    output = {
+                        'user_name': user.member,
+                        'user_age': user.age,
+                        'user_city': user.city,
+                        'user_job': user.job
+                    }                    
+                    with open('UserContactPageData.json', 'a') as d:
+                        json.dump(output, d, indent=4)
+                    return render(request, 'contact.html', output)
+    return render(request, 'contact.html')
+
+
+def index(request):
+    name = request.GET.get('u_name')
+    age = request.GET.get('u_age')
+    city = request.GET.get('u_city')
+    job = request.GET.get('u_job')
+    if name:
+        if age:
+            if city:
+                if job:
+                    class model():
+                        def __init__(self, member, age, city, job):
+                            self.member = member
+                            self.age = age
+                            self.city = city
+                            self.job = job
+
+                        def member_name(self):
+                            return self.member
+
+                        def member_age(self):
+                            return self.age
+
+                        def member_city(self):
+                            return self.city
+
+                        def member_job(self):
+                            return self.job
+
+                    user = model(name, age, city, job)
+
+                    output = {
+                        'user_name': user.member,
+                        'user_age': user.age,
+                        'user_city': user.city,
+                        'user_job': user.job
+                    }                    
+                    with open('UserData.json', 'a') as d:
+                        json.dump(output, d, indent=4)
+                    return render(request, 'index.html', output)
+    return render(request, 'index.html')
